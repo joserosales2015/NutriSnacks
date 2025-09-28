e9fc87d4610a466d8b6c73fbef6e72d5ec6b416d
Jose Rosales <joserosales2015@gmail.com>
2025-09-27 23:10:11 -0500

    Envio inicial

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+const usuarios = [
+    { id: 1, nombre: 'Juan', edad: 28 },
+    { id: 2, nombre: 'María', edad: 34 },
+    { id: 3, nombre: 'Pedro', edad: 22 },
+]
+
+app.get('/', (req, res) => {
+    res.send('¡Servicios de NutriSnacks!');
+});
+
+app.get('/usuarios', (req, res) => {
+    res.json(usuarios);
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
